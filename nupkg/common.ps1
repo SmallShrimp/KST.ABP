1f5b75df77c370d812c155aa8359d7b8b0c6cb0e
gexiaoxu <gexiaoxu@126.com>
2020-01-14 10:26:18 +0800

    Nuget版本

---
+++ nupkg/common.ps1
@@ -0,0 +1,24 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+$rootFolder = Join-Path $packFolder "../"
+
+# List of solutions
+$solutions = (
+    "Organizations"
+)
+
+# List of projects
+$projects = (
+
+    # Organizations
+	"Organizations/src/KST.ABP.Organizations.Domain.Shared",
+	"Organizations/src/KST.ABP.Organizations.Domain",    
+	"Organizations/src/KST.ABP.Organizations.Application.Contracts",
+	"Organizations/src/KST.ABP.Organizations.Application",	
+	"Organizations/src/KST.ABP.Organizations.EntityFrameworkCore",
+	"Organizations/src/KST.ABP.Organizations.HttpApi",
+	"Organizations/src/KST.ABP.Organizations.HttpApi.Client",
+	"Organizations/src/KST.ABP.Organizations.MongoDB",
+	"Organizations/src/KST.ABP.Organizations.Web"
+    
+)
